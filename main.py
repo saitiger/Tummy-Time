bbccbc476a3365ec0dae125268509b84c5077878
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-09-04 15:44:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-from util import process_dataset, overall_class_stats  # Import the function from util
+import time
+from util import process_dataset, overall_class_stats 
 
 st.set_page_config(layout="wide")
 
@@ -10,12 +11,16 @@ if 'df' not in st.session_state:
 uploaded = st.file_uploader(label='Upload the CSV file', type='csv')
 
 if uploaded is not None:
-    df = process_dataset(uploaded)
+    with st.spinner("Processing"):
+        df = process_dataset(uploaded)
     
     if isinstance(df, str):  
         st.error(df)
     else:
-        st.success('File successfully uploaded and processed.')
+        message_placeholder = st.empty()
+        message_placeholder.success('File successfully uploaded and processed.')
+        time.sleep(3)
+        message_placeholder.empty()  # Clear the success message after 3 seconds
         st.session_state.df = df  
 
     #---- SIDEBAR ----
@@ -46,25 +51,31 @@ if uploaded is not None:
 
     st.dataframe(df_selection)
 
-    col1, col2 = st.columns(2)
-    if col1.button("View Plots"):
-        st.switch_page("pages/Plots.py")
-    if col2.button("View EDA"):
-        st.switch_page("pages/EDA.py")
+    col1, col2,col3 = st.columns(3)
+    with col1:
+        if col1.button("View Plots"):
+            st.switch_page("pages/1_Plots.py")
+    with col2:
+        if col2.button("View EDA"):
+            st.switch_page("pages/2_EDA.py")
+    with col3:
+        if col3.button("View Blocks"):
+            st.switch_page("pages/3_Blocks.py")
 
-    if st.sidebar.button("Generate Class Statistics"):
-        overall_class = st.sidebar.selectbox("Select Overall Class for Stats", options=df["Overall class"].unique())
+    # Depreciated 
+    # if st.sidebar.button("Generate Class Statistics"):
+    #     overall_class = st.sidebar.selectbox("Select Overall Class for Stats", options=df["Overall class"].unique())
         
-        file_path = 'results.txt'
-        cnt_arr, max_sequence = overall_class_stats(df, overall_class, file_path)
+    #     file_path = 'results.txt'
+    #     cnt_arr, max_sequence = overall_class_stats(df, overall_class, file_path)
         
-        with open(file_path, 'r') as file:
-            st.download_button(
-                label="Download Class Statistics",
-                data=file,
-                file_name=file_path,
-                mime='text/plain'
-            )
+#         with open(file_path, 'r') as file:
+#             st.download_button(
+#                 label="Download Class Statistics",
+#                 data=file,
+#                 file_name=file_path,
+#                 mime='text/plain'
+#             )
 
 else:
     st.warning("Please upload a CSV file to get started.")
